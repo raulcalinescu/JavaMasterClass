2c0df753de2b40ee9766fc5bfdeb6435d0a0b4a8
raulcalinescu <raul.salajan@gmail.com>
2019-08-03 14:54:36 -0500

    PERSONAL LAPTOP

--- src/section5/controlflow/statements/PrimeNumbers.java
+++ src/section5/controlflow/statements/PrimeNumbers.java
@@ -5,10 +5,25 @@ public class PrimeNumbers {
     public static boolean isPrime (int n) {
         if(n == 1) return false;
 
-        for(int i = 0; i <= Math.sqrt(n); i++) {
+        for(int i = 2; i <= Math.sqrt(n); i++) {
             if(n % i == 0) return false;
         }
 
         return true;
     }
+
+
+    //print only the even numbers. NOTE i+=2 in for loop
+
+    public static void printEven () {
+        for (int i = 0; i < 100; i+=2) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printEven();
+        System.out.println(isPrime(34));
+    }
+
 }
