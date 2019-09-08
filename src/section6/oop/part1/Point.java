bb44413e29682e27e262c5759218b5b243408989
raulcalinescu <raul.salajan@gmail.com>
2019-09-08 12:51:11 -0500

    Personal Laptop

---
+++ src/section6/oop/part1/Point.java
@@ -0,0 +1,45 @@
+package section6.oop.part1;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+        System.out.print("Empty Constructor called");
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {  // Calculate distance from this point to a point zero
+        Point zero = new Point(0,0);
+        return Math.sqrt(Math.pow((zero.x - this.x),2) + Math.pow((zero.y - this.y),2));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow((x - this.x),2) + Math.pow((y - this.y),2));
+    }
+
+    public double distance(Point another) {
+        return Math.sqrt(Math.pow((another.getX() - this.x),2) + Math.pow((another.getY() - this.y),2));
+    }
+}
