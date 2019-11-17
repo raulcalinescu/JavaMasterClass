f75e408f9bb89452efb9bdb2a4a5da60ddbf448e
raulcalinescu <raul.salajan@gmail.com>
2019-11-17 07:58:37 -0600

    Personal Laptop

---
+++ src/section8/arrays_lists_autoboxing/MinElemChallenge.java
@@ -0,0 +1,40 @@
+package section8.arrays_lists_autoboxing;
+
+import java.util.Scanner;
+
+public class MinElemChallenge {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the count of integers to be added into the array! \n");
+
+        int count = scanner.nextInt();
+        scanner.nextLine();                    // follow with this statement to process the 'Enter' key
+        int[] arr = readIntegers(count);
+        int min = findMin(arr);
+        System.out.println("The minimum number in the array is " + min);
+    }
+
+    public static int[] readIntegers(int count) {
+        int[] array = new int[count];
+
+        while (count > 0) {      //if using a for loop you use another address in memory for index and no need for that
+            System.out.println("Enter a value to be added to the array: \n");
+            array[count-1] = scanner.nextInt();
+            scanner.nextLine();
+            count--;
+        }
+        return array;
+    }
+
+    public static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+    }
+
+}
--- src/section8/arrays_lists_autoboxing/Practice.java
+++ src/section8/arrays_lists_autoboxing/Practice.java
@@ -2,12 +2,20 @@ package section8.arrays_lists_autoboxing;
 
 public class Practice {
 
+
+
+
     public static void main(String[] args) {
         int[] myIntArray = new int[10];
+        int[] myNumbers = {1,2,3,4,5};
         myIntArray[5] = 50;
 
         for (int i = 0; i < 10; i++) {
             System.out.print(myIntArray[i] + " ");
+        }
+        myNumbers[2] = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(myNumbers[i]);
         }
 
     }
